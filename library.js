91b0be6130d6fb6489548482be2ca589cad4a601
DivyanshKushwaha2023 <divyanshkushwaha2407@gmail.com>
2024-08-15 13:07:01 +0530

    Create addToDom() to display books on the screen
    Created a function addToDom() that displays the books in library array to the screen

--- library.js
+++ library.js
@@ -14,3 +14,20 @@ function Books(name, author, pages, read) {
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
+
+//Manually adding books to see output
+const one = new Books("one", "auth1", 1, "read");
+const two = new Books("two", "auth2", 2,"not read");
+
+addBookToLibrary(one);
+addBookToLibrary(two);
+
+function addToDom() {
+    let currentTable = document.querySelector('#row');
+    myLibrary.forEach(book => {
+        let newRow = `<tr><td>${book.name}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.read}</td></tr>`;
+        currentTable.innerHTML += newRow;
+    })
+}
+
+addToDom();
